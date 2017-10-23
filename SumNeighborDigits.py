934827b1f289c4a78b8b3fc678e186f787f5bc31
agcodes-git <zoldy.wf@gmail.com>
2017-10-23 16:24:53 -0600

    Now uses all 256 1-neighbor rules for 1D automata, but doesn't allow for dynamically changing sets for repeated application of said rules. Will fix later.

--- SumNeighborDigits.py
+++ SumNeighborDigits.py
@@ -4,35 +4,48 @@ import matplotlib.pyplot as plt
 def num2list(n,b):
     if b == 2: return([int(x) for x in list("{0:b}".format(n))])
     elif b == 10: return([int(x) for x in list("{0:d}".format(n))])
-
-
 def list2num(l,b):
     n = 0
     for x in range(0,len(l)):
         n += math.pow(b,x)*int(l[len(l)-x-1])
     return n
+def repeat(f, n):
+     if n==0:
+             return (lambda x: x)
+     return (lambda x: f (int(repeat(f, n-1)(x))))
 
-# Note that the order of magnitude won't change.
-def pairsum(n,b):
-    l = num2list(n,b)
-    pl = [] if len(l) != 1 else l
-    if pl != l:
-        pl.append( (0+l[0]+l[1]) %b)
-        for x in range(1,len(l)-1):
-            pl.append( (l[x-1]+l[x]+l[x+1]) %b)
-        if len(l)>2: pl.append( (l[len(l)-2]+l[len(l)-1]+0) %b)
-    return list2num( pl,b )
+# The base of the number is analogous to the size of the neighborhood.
+# The number of digits is analogous to the number of CA.
+base = 2
+k = 512
 
-base = 10
-k = 100000
-plt.plot(range(0,k), [pairsum(x,base) for x in range(0,k)])
+# For binary. Map the result of the application of a rule to the original set of CA, expressed as a number.
+def apply_rule(n,rule_number):
+    s = num2list(n,2) # This is our set of starting automata.
+    ns = [] # The new set. Empty at first.
 
-plt.ylabel('triplet digit sum, mod '+str(base))
+    #print('Starting set: ', s)
+
+    rule = num2list(rule_number,2)
+    while len(rule)<8: rule.insert(0,0)
+    #print('Rule',rule_number,rule)
+
+    # We will assume that the edge elements are 0s.
+    #print('Left edge: ', [0]+s[0:2])
+    ns.append( int(rule[int(list2num([0]+s[0:2],2))]))
+    for x in range(1,len(s)-1):
+        set_of_3 = s[x-1:x+2]
+        ns.append( int(rule[int(list2num(s[x-1:x+2],2))]) )
+        #print(set_of_3)
+    #print('Right edge: ', s[len(s)-2:len(s)]+[0])
+    ns.append( int(rule[int(list2num(s[len(s)-2:len(s)]+[0] , 2))]))
+    return list2num(ns,2)
+
+iterations = 5 # How many times should we apply the rule to each set of cellular automata?
+                # Keep in mind that currently one can't propagate outside of the magnitude borders.
+
+for r in range(0,255):
+    composed_rule = repeat( lambda x: apply_rule(x,r), 1 ) # Curry and repeatedly compose apply_rule.
+    plt.plot( range(0,100), [composed_rule(x) for x in range(0,100)])
+
 plt.show()
-
-# Every power of the base, the pattern repeats.
-# The pattern for 10-100 is the pattern of 1-10 repeated 10 times plus
-# the pattern of 1-10 slowed down by a factor of 10 and increased in amplitude
-# by a factor of 10. Super-positioned fractal 'waves' for each period.
-# Hence it is easy to see how changing the number of actors in a set of
-# CA changes the first step.
